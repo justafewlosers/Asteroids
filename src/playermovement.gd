ae19df9479321b60877091ee84ed49da77b70cb1
SPQRpunk <tmannpunk@gmail.com>
2021-05-14 02:43:35 -0400

    Reworked movement

--- src/playermovement.gd
+++ src/playermovement.gd
@@ -1,28 +1,31 @@
 extends RigidBody2D
 
 const UP = Vector2(0, 1)
-const MAX_VEL = 400
-const MAX_TORQUE = 20
 
 var thrust = Vector2()
-var engine_thrust = 10
-var spin_thrust = 0.01
+var engine_thrust = 15
+var spin_thrust = 0.4
 var rot_dir = 0
 
-func get_input(delta):
+func thruster():
 	thrust = Vector2(0, 0)
 	if Input.is_action_pressed("up"):
 		thrust = Vector2(engine_thrust, 0)
 	else:
 		thrust = Vector2(max(thrust.x - 25, 0), 0)
-
-	rot_dir = 0
+		
+func rotate(delta):
+	rot_dir = 0.0
 	if Input.is_action_pressed("right"):
-		rot_dir = min(rot_dir - 1, -10) * delta
+		rot_dir = min(rot_dir + 1, 10) * delta
 	elif Input.is_action_pressed("left"):
-		rot_dir = max(rot_dir + 1, 10) * delta
+		rot_dir = max(rot_dir - 1, -10) * delta
 	else:
-		rot_dir = lerp(rot_dir, 0, 10)
+		rot_dir = lerp(rot_dir, 0, 12)
+	
+func get_input(delta):
+	thruster()
+	rotate(delta)
 
 func _physics_process(delta):
 	get_input(delta)
