d8de7d168762e17bac496cb8c005056d292fc5bf
bobspenis <bobsthepenis@gmail.com>
2021-05-13 22:01:59 -0400

    added friction

--- src/playermovement.gd
+++ src/playermovement.gd
@@ -1,35 +1,29 @@
 extends KinematicBody2D
 
 const UP = Vector2(0,-1)
+const MAX_VEL = 400
 
 var rot_dir = 0
 var rot_speed = 5
-var velocity = Vector2()
+var motion = Vector2()
 var accel = 50
-var max_vel = 400
+var base_vel = 0
 
-func add_velocity():
-	if velocity.y < max_vel:
-		velocity.y += -accel
-		velocity = Vector2(0, -velocity.y).rotated(rotation)
-		
-
-func velocity():
-	if velocity.y > 0:
-		velocity.y = lerp(0, velocity.y, 0.2)
-	
 func get_input():
 	rot_dir = 0
+#	motion = Vector2()
+#	motion.y = lerp(motion.y, 0, 0.1)
+#	motion.x = lerp(motion.x, 0, 0.1)
 	if Input.is_action_pressed("right"):
 		rot_dir += 1
 	if Input.is_action_pressed("left"):
 		rot_dir -= 1
 	if Input.is_action_pressed("up"):
-		add_velocity()
-	else:
-		velocity()
-		
+		motion = Vector2(0, max(abs(motion.y) + accel, MAX_VEL)).rotated(rotation)  * UP.y
+	elif abs(motion.y) > 0:
+		motion = Vector2(lerp(abs(motion.x), 0, 0.1), lerp(abs(motion.y), 0, 0.1)).rotated(rotation)
+
 func _physics_process(delta):
 	get_input()
 	rotation += rot_dir * rot_speed * delta
-	velocity = move_and_slide(velocity)
+	motion = move_and_slide(motion)
