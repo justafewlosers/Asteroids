28200164d0363fe3d25d5a7309196fb53481f97b
SPQRpunk <tmannpunk@gmail.com>
2021-05-15 04:36:59 -0400

    Added projectile max range and wraparound.

--- src/blast.gd
+++ src/blast.gd
@@ -2,6 +2,27 @@ extends Area2D
 
 var speed = 1500
 
+onready var screen_size = get_viewport_rect().size
+
+func _ready():
+	var t = Timer.new()
+	t.set_wait_time(0.4)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	queue_free()
+		
 func _physics_process(delta):
+	screenwrap()
 	position += transform.x * speed * delta
 
+func screenwrap():
+	if position.x > screen_size.x:
+		position.x = 0
+	if position.x < 0:
+		position.x = screen_size.x
+	if position.y > screen_size.y:
+		position.y = 0
+	if position.y < 0:
+		position.y = screen_size.y
