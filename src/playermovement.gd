e2c9c4ae238a17e40b1c70371cf993ea280224ee
SPQRpunk <tmannpunk@gmail.com>
2021-05-14 21:24:11 -0400

    Added shooting and sounds.

--- src/playermovement.gd
+++ src/playermovement.gd
@@ -1,23 +1,58 @@
 extends RigidBody2D
 
+export (PackedScene) var bullet
+
 const UP = Vector2(0, 1)
 const ENGINE_THRUST = 100
-const SPIN_THRUST = 15
+const SPIN_THRUST = 12
 
 var thrust = Vector2()
 var rot_dir = 0
 var screensize
+var sound_played = false
+
+var attack_cooldown = 100
+var next_attack_time = 0
+var burst = 0
+var burst_cap = 3
+var burst_cd = 300
 
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
+
+func blaster():
+	if Input.is_action_pressed("shoot"):
+		shoot()
+
+func shoot():
+	var b = bullet.instance()
+	var now = OS.get_ticks_msec()
+	if now >= next_attack_time:
+		if burst < burst_cap:
+			owner.add_child(b)
+			b.transform = $blaster.global_transform
+			next_attack_time = now + attack_cooldown
+			burst = burst + 1
+		elif burst == burst_cap:
+			next_attack_time = now + burst_cd
+			burst = 0
+		
+func thrust_sound():
+	if Input.is_action_just_pressed("up"):
+		if !sound_played:
+			sound_played = true
+			$AudioStreamPlayer.play()
 	
 func thruster():
 	thrust = Vector2(0, 0)
 	if Input.is_action_pressed("up"):
 		thrust = Vector2(ENGINE_THRUST, 0)
 		$plume.visible = true
+		thrust_sound()
 	else:
 		$plume.visible = false
+		$AudioStreamPlayer.stop()
+		sound_played = false
 		
 func rotate(delta):
 	rot_dir = 0.0
@@ -31,6 +66,7 @@ func rotate(delta):
 func get_input(delta):
 	thruster()
 	rotate(delta)
+	blaster()
 	
 func _physics_process(delta):
 	get_input(delta)
